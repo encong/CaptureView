47881b2bfd59c7a3469d6b9f6eaa8e48a3347fa5
于恩聪 <yuencong@ppzuche.com>
2016-08-12 06:08:37 +0800

    Initial Commit

---
+++ CaptureView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaptureView
+//
+//  Created by ppzc on 16/8/12.
+//  Copyright © 2016年 ppzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CaptureView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaptureView
+//
+//  Created by ppzc on 16/8/12.
+//  Copyright © 2016年 ppzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
